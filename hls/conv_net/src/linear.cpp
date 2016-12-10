106ac59de1adad9dd280f794361b242653dc4679
Will Zegers <will.zegers@gmail.com>
2016-12-09 21:18:51 -0800

    Moved flatten code into linear.cpp

--- hls/conv_net/src/linear.cpp
+++ hls/conv_net/src/linear.cpp
@@ -1,6 +1,23 @@
 #include "conv_net.h"
 #include <stdint.h>
 
+void flatten(
+		DTYPE IN[C2_N_FILTERS][P2_DOWNSIZE][P2_DOWNSIZE],
+		DTYPE OUT[FLAT_VEC_SZ]) {
+
+	uint8_t i,j,k;
+	uint16_t t;
+
+	for (i = 0, t = 0; i < C2_N_FILTERS; ++i) {
+		for (j = 0; j < P2_DOWNSIZE; ++j) {
+			for (k = 0; k < P2_DOWNSIZE; ++k) {
+				OUT[t] = IN[i][j][k];
+				t += 1;
+			}
+		}
+	}
+}
+
 void vec_mat_mul_f1(
 	      DTYPE X[FLAT_VEC_SZ],
 	const DTYPE W[F1_ROWS][F1_COLS],
